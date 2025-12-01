8b99bcc3de8f85eede586e0b5898444821515854
cqkevin <qchen534@uwo.ca>
2025-12-01 09:04:15 -0500

    Models add Decision Tree

--- Models.py
+++ Models.py
@@ -6,8 +6,11 @@ import warnings
 warnings.filterwarnings('ignore')
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-import sklearn.metrics as metrics
-from sklearn.metrics import r2_score
+from sklearn.tree import DecisionTreeRegressor
+import sklearn as metrics
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+
+from Processing import CleanData
 
 def multipleLinearRegression(df = pd.read_csv('car_sales_data.csv')):
     
@@ -55,3 +58,48 @@ def multipleLinearRegression(df = pd.read_csv('car_sales_data.csv')):
     #print('Mean Square Error:', mse)
     #print('Root Mean Square Error:', r2)
 
+def decisionTree(test_size = 0.3, random_state = 100):
+    DT = CleanData().copy()
+    DT["Car_Age"] = 2025 - DT["Year"]
+    DT.drop('Year', axis=1, inplace=True)
+
+    categorical_cols = ["Manufacturer", "Model", "Fuel_Type"]
+    df_encoded = pd.get_dummies(DT, columns=categorical_cols, drop_first=True)
+
+    # 5. 拆分 X / y
+    X = df_encoded.drop("Price", axis=1)
+    y = df_encoded["Price"].values
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state)
+    model = DecisionTreeRegressor(
+        criterion="squared_error",
+        max_depth=10,
+        min_samples_split=10,
+        min_samples_leaf=5,
+        random_state=random_state,
+    )
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+
+    MAE = mean_absolute_error(y_test, y_pred)
+    MSE = mean_squared_error(y_test, y_pred)
+    RMSE = np.sqrt(MSE)
+    R2 = r2_score(y_test, y_pred)
+
+    metrics_dict = {
+        "MAE": MAE,
+        "MSE": MSE,
+        "RMSE": RMSE,
+        "R2": R2,
+    }
+
+    results_df = pd.DataFrame({
+        "Actual": y_test,
+        "Predicted": y_pred,
+    })
+
+    print("\n=== Decision Tree Regression Results ===")
+    for k, v in metrics_dict.items():
+        print(f"{k}: {v:.4f}")
+
+    return model, metrics_dict, results_df
