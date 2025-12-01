abff33ec4e6bf69586776f83cdc0f889fd386051
Mohammad-Tareq Assourah <massoura@uwo.ca>
2025-11-30 22:29:23 -0500

    CleanData and Models before Training Pushed

--- Processing.py
+++ Processing.py
@@ -1,1 +1,18 @@
+import pandas as pd
+
+def CleanData():
+  df = pd.read_csv("car_sale_data.csv")
+
+  df = df.drop_duplicates()
+
+  df = df.dropna(subset=["price"])
+
+# Simple fill for missing data
+  df = df.fillna(method="ffill")
+
+# Reset index
+  df = df.reset_index(drop=True)
+
+  return df
+
 
