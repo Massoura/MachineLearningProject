806346762ec1e90db1470df9ad768424a636a837
cqkevin <qchen534@uwo.ca>
2025-12-01 11:49:22 -0500

    Models add Gradient Boosting

--- Models.py
+++ Models.py
@@ -10,7 +10,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
-from
 import sklearn as metrics
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 
